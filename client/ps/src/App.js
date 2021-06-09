e014eb05c596d5fb49ffec7ebe4622bcfd6f28f2
ilhmpy <vledvled989@gmail.com>
2021-06-09 20:15:58 +0300

    20:15. 09.06.2021. Add main page, and navigation in project.

--- client/ps/src/App.js
+++ client/ps/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Main from "./pages/Main.jsx";
 import PrivateRouter from "./components/PrivateRouter.jsx";
+import "./Global.css";
 
 export default function App() {
   return (
--- client/ps/src/pages/Main.jsx
+++ client/ps/src/pages/Main.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import "./styles/Main.css";
 
 export default function Main() {
   return (
-    <div className="main"></div>
+    <div className="main">
+      <div className="container">
+        <div className="main__about">
+          <h1><span>Front-end</span> разработка</h1>
+          <p>
+            Качественная и быстрая работа. Постоянная связь и заинтересованность в прекрасном результате.
+          </p>
+        </div>
+        <div className="main__form">
+          
+        </div>
+      </div>
+    </div>
   );
 };
