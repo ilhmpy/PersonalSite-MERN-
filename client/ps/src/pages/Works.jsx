accb5d91ddb6d6bde4fe86b7a87922d4154514de
ilhmpy <vledvled989@gmail.com>
2021-06-10 21:37:59 +0300

    21:37. 10.06.2021.

--- client/ps/src/pages/Works.jsx
+++ client/ps/src/pages/Works.jsx
@@ -10,7 +10,19 @@ export default function Works() {
       <div className="works">
         <div className="container">
           <div className="works__content">
-            
+            <h1 className="works__content_title">YOOF</h1>
+            <div className="works__content_imgs">
+              <img src="" className="works__content_img" />
+              <img src="" className="works__content_img" />
+              <img src="" className="works__content_img" />
+            </div>
+            <p className="works__content_text">
+              YOOF - сайт который нацелен на помощь людям в поиске самых выгодных предложений. С помощью аукциона.
+            </p>
+            <div className="works__content_links">
+              <a href="#" target="_blank"><i class="fab fa-github"></i></a>
+              <a href="#" target="_blank">На хостинге</a>
+            </div>
           </div>
         </div>
       </div>
